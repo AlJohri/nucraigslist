cbfebec76874c0ad75b95bac2cf491846bd19c22
Al Johri <al.johri@gmail.com>
2014-11-06 12:58:18 -0600

    add category column to admin interface

--- listings/admin.py
+++ listings/admin.py
@@ -14,7 +14,7 @@ class ListingsInline(admin.TabularInline):
 class ListingAdmin(admin.ModelAdmin):
 	fields = ['id', 'created_time', 'updated_time', 'type', 'message', 'seller', 'approved', 'buy_or_sell', 'category', 'sold']
 	list_filter = ['buy_or_sell', 'created_time', 'updated_time']
-	list_display = ['id', 'message', 'created_time']
+	list_display = ['id', 'message', 'category', 'created_time']
 	actions = [make_approved]
 	inlines = [CommentsInline]
 
