47927745b1e85fe351168874d50ed458d568de16
Al Johri <al.johri@gmail.com>
2014-11-10 02:10:33 -0600

    redirect homepage

--- listings/urls.py
+++ listings/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import *
 from listings import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<listing_id>[0-9]+)/$', views.detail, name='detail')
+	url(r'^$', views.index, name='index')
 )
