8604e1dc26b61d4d881ba49533f97c3d8898498d
Al Johri <al.johri@gmail.com>
2014-11-11 10:13:43 -0600

    change database to use postgres

--- freeandforsale/settings.py
+++ freeandforsale/settings.py
@@ -60,18 +60,18 @@ WSGI_APPLICATION = 'freeandforsale.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 import dj_database_url
-# export DATABASE_URL="sqlite://db.sqlite3"
-# DATABASES = {}
-# DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
