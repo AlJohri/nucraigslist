e121947d3416a61b3b5cbf26752f597109ff0f82
Al Johri <al.johri@gmail.com>
2014-11-17 03:35:38 -0600

    convert backup to be with create statements

--- listings/management/commands/sqlclear.py
+++ listings/management/commands/sqlclear.py
@@ -11,9 +11,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-		cursor = connection.cursor()
-		cursor.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
-		parts = ('DROP TABLE IF EXISTS %s;' % table for (table,) in cursor.fetchall())
-		sql = 'SET FOREIGN_KEY_CHECKS = 0;\n' + '\n'.join(parts) + '\n' + 'SET FOREIGN_KEY_CHECKS = 1;\n'
+        """
+        this command is a work in progress, please use carefully
+        """
 
-		print sql
+        cursor = connection.cursor()
+        cursor.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
+        parts = ('DROP TABLE IF EXISTS %s CASCADE;' % table for (table,) in cursor.fetchall())
+        # sql = 'SET FOREIGN_KEY_CHECKS = 0;\n' + '\n'.join(parts) + '\n' + 'SET FOREIGN_KEY_CHECKS = 1;\n'
+
+        print '\n'.join(parts)
